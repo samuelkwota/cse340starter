8511fd0563db0d0b89073b3102acad7365b56a46
samuelkwota <samuelkwota.sk@gmail.com>
2024-10-22 17:50:50 -0800

    sql

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,23 +1,23 @@
 -- Task 1: Insert a new record into the account table
-INSERT INTO account (first_name, last_name, email, password)
+INSERT INTO account (first_name, last_name, email_address, password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
 -- Task 2: Update Tony Stark's account type to "Admin"
 UPDATE account
 SET account_type = 'Admin'
-WHERE email = 'tony@starkent.com';
+WHERE email_address = 'tony@starkent.com';
 
 -- Task 3: Delete the Tony Stark record from the account table
 DELETE FROM account
-WHERE email = 'tony@starkent.com';
+WHERE email_address = 'tony@starkent.com';
 
 -- Task 4: Update the description of the "GM Hummer" record
 UPDATE inventory
-SET description = REPLACE(description, 'small interiors', 'a huge interior')
-WHERE model = 'GM Hummer';
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_model = 'GM Hummer';
 
 -- Task 5: Use an INNER JOIN to select make, model, and classification name for "Sport" category
-SELECT inventory.make, inventory.model, classification.classification_name
+SELECT inv_make, inv_model, classification.classification_name
 FROM inventory
 INNER JOIN classification ON inventory.classification_id = classification.classification_id
 WHERE classification.classification_name = 'Sport';
